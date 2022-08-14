82d8fbf0fb3edd2fa3c76b6fbe205e7adff1daf6
Antonio Alves <tonyalveslopes@gmail.com>
2022-08-14 12:24:42 -0300

    fix error in sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 git clone https://github.com/mozilla/DeepSpeech
 docker run --gpus all -v ~/deepspeech/DeepSpeech/:/deep -it --entrypoint bash tensorflow/tensorflow:1.15.4-gpu-py
