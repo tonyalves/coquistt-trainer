682295806f9f1159023970fd20241302c47001bc
Antonio Alves <tonyalveslopes@gmail.com>
2022-08-14 12:23:14 -0300

    first

---
+++ run_docker.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+
+git clone https://github.com/mozilla/DeepSpeech
+docker run --gpus all -v ~/deepspeech/DeepSpeech/:/deep -it --entrypoint bash tensorflow/tensorflow:1.15.4-gpu-py
---
+++ trainer.sh
@@ -0,0 +1,1 @@
+python3 DeepSpeech.py --train_files ./cv-corpus-7.0-2021-07-21/pt/clips/train.csv --dev_files ./cv-corpus-7.0-2021-07-21/pt/clips/dev.csv --test_files ./cv-corpus-7.0-2021-07-21/pt/clips/test.csv --alphabet_config_path alphabet.txt --train_batch_size 100 --dev_batch_size 100 --test_batch_size 50 --epochs 100
